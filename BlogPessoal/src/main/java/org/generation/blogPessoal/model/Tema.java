cba9fb7137f1f461783e8d4da066a880e62c65c1
unknown <ferreirasousajaqueline@gmail.com>
2021-10-02 03:59:28 -0300

    Criando projeto com angular

---
+++ BlogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -0,0 +1,44 @@
+package src.main.java.org.generation.blogPessoal.model;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String descricao;
+	
+	@OneToMany(mappedBy = "tema", cascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+	
+	
+
+}
